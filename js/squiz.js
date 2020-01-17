f99d0c1557438ad23724e8b37ac36866852940cd
Burak Seyhan <seyhanburak9@gmail.com>
2020-01-17 22:58:58 +1100

    squiz assessment added

--- js/squiz.js
+++ js/squiz.js
@@ -18,23 +18,3 @@ data().map(t => {
 
 // loop inside the new sorted array and append the values with related list items
 sortedData.map(f => $(".fruit-list").append("<li>" + f + "</li>"));
-const data = () => [
-  { title: "Apples" },
-  { title: "Oranges" },
-  { title: "Bananas" },
-  { title: "Kiwi Fruit" },
-  { title: "Cherries" }
-];
-
-// create an empty array
-const sortedData = [];
-
-// push the relevant data into the empty array
-data().map(t => {
-  sortedData.push(t.title);
-  // sort the array data with alfhabetical order
-  sortedData.sort();
-});
-
-// loop inside the new sorted array and append the values with related list items
-sortedData.map(f => $(".fruit-list").append("<li>" + f + "</li>"));
